34716e5b839c3172466ae372cce89561027e2367
shanmugam bhaskaran <shanmugam.bhaskaran@oup.com>
2022-02-22 12:33:42 +0000

    initial version

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function(AppComponent) {
+    return AppComponent.extend("oup.rtr.vattaxdeletion.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
